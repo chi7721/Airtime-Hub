030b06be5a43be4d5f337fa5314c121ba15342f4
chi7721 <197515507+chi7721@users.noreply.github.com>
2025-04-08 00:01:07 -0700

    Map added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+$(".window").draggable();
 
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-    $("#filters-window").draggable();
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
