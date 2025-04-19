ca1bf6dec0e4a27bd8659bd41e98e37cacda9748
chi7721 <197515507+chi7721@users.noreply.github.com>
2025-04-19 15:17:20 -0700

    Alpha front end done

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var markerTemplate = document.getElementById('marker-template').innerHTML;
+var customIcon = L.divIcon({
+    className: 'vector-parent',
+    html: markerTemplate,
+    iconSize: [21.3, 27],
+    iconAnchor: [10.65, 27]
+});
+L.marker([54, 0], {
+    icon: customIcon
+}).addTo(map);
